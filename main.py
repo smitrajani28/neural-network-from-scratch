572f8fbb3a90ea7863b94f997ebde805c3c0d078
Smitrajani28 <smitrajani28@gmail.com>
2024-07-12 12:08:51 +0530

    working project added further changes remaining

---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+from dataset import create_data
+np.random.seed(0)
+
+x,y = create_data(100,3)
+
+class Layer_dense:
+    def __init__(self,n_input,n_nurons):
+        self.weights =0.1 * np.random.randn(n_input,n_nurons)
+        self.biases = np.zeros((1,n_nurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.biases
+
+class Activate_ReLU:
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activate_Softmax:
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values,axis=1,keepdims=True)
+        self.output = probabilities
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) 
+
+        if len(y_true.shape) == 1 :
+            correct_confidence = y_pred_clipped[range(samples),y_true]
+        
+        elif len(y_true.shape) == 2:
+            correct_confidence = np.sum(y_pred_clipped*y_true,axis=1)
+        
+        negative_log_likelihoods = -np.log(correct_confidence)
+        return negative_log_likelihoods
+        
+
+
+layer1 = Layer_dense(2,3)
+activation1 = Activate_ReLU()
+
+layer2 = Layer_dense(3,3)
+activation2 = Activate_Softmax()
+
+layer1.forward(x)
+activation1.forward(layer1.output)
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
+print(activation2.output[:5])
